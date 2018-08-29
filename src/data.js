f9a72e7e64662acf3ef8748760a61c870ffc5223
Andrew Hope <ahope@redventures.com>
2018-08-29 16:26:57 -0700

    data

--- src/data.js
+++ src/data.js
@@ -28,26 +28,126 @@ const Data = [
     name: 'Omega-3',
     amount: '320 MG',
     origin: 'South Carolina, USA',
+    detail: {
+      form: '320 MG ALPHA-LINOLENIC ACID',
+      manufacturer: 'PharmaVita',
+      found: 'Shrimp, Scallops, Clams, Tilapia, Cod.',
+      image: 'https://www.sciencedaily.com/images/2018/07/180717194558_1_540x360.jpg',
+      icons: [
+        {
+          source: 'https://image.flaticon.com/icons/svg/1048/1048706.svg',
+          name: 'BLOOD FLOW',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/116/116338.svg',
+          name: 'BRAIN HEALTH',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/126/126471.svg',
+          name: 'HEART HEALTH',
+        },
+      ],
+    },
   },
   {
     name: 'Vitamin D3',
     amount: '2000 IU',
     origin: 'United Kingdom',
+    detail: {
+      form: '10MG CALCITRIOL LIQUID',
+      manufacturer: 'BARMETRIC',
+      found: 'Fish, Eggs, and Cheese.',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Cholecalciferol.svg/1200px-Cholecalciferol.svg.png',
+      icons: [
+        {
+          source: 'https://image.flaticon.com/icons/svg/84/84442.svg',
+          name: 'BONE STRENGTH',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/126/126471.svg',
+          name: 'HEART HEALTH',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/811/811932.svg',
+          name: 'BLOOD PRESSURE',
+        },
+      ],
+    },
   },
   {
     name: 'Iron',
     amount: '8 MG',
     origin: 'Utah, USA',
+    detail: {
+      form: '5MTHF GLUCOSAMINE SALT',
+      manufacturer: 'Gnosis',
+      found: 'Lentils, avocado, and oranges.',
+      image: 'https://5.imimg.com/data5/EX/IU/MY-2290318/folic-acid-250x250.jpg',
+      icons: [
+        {
+          source: 'https://image.flaticon.com/icons/svg/39/39272.svg',
+          name: 'MOOD',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/340/340624.svg',
+          name: 'DNA SYNTHESIS',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/109/109727.svg',
+          name: 'BRAIN HEALTH',
+        },
+      ],
+    },
   },
   {
     name: 'Vitamin K2',
     amount: '90 MCG',
     origin: 'Oslo, Norway',
+    detail: {
+      form: '320 MG ALPHA-LINOLENIC ACID',
+      manufacturer: 'PharmaVita',
+      found: 'Shrimp, Scallops, Clams, Tilapia, Cod.',
+      image: 'https://www.sciencedaily.com/images/2018/07/180717194558_1_540x360.jpg',
+      icons: [
+        {
+          source: 'https://image.flaticon.com/icons/svg/1048/1048706.svg',
+          name: 'BLOOD FLOW',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/116/116338.svg',
+          name: 'BRAIN HEALTH',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/126/126471.svg',
+          name: 'HEART HEALTH',
+        },
+      ],
+    },
   },
   {
     name: 'Magnesium',
     amount: '50 MG',
     origin: 'Pisa, Italy',
+    detail: {
+      form: '10MG CALCITRIOL LIQUID',
+      manufacturer: 'BARMETRIC',
+      found: 'Fish, Eggs, and Cheese.',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Cholecalciferol.svg/1200px-Cholecalciferol.svg.png',
+      icons: [
+        {
+          source: 'https://image.flaticon.com/icons/svg/84/84442.svg',
+          name: 'BONE STRENGTH',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/126/126471.svg',
+          name: 'HEART HEALTH',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/811/811932.svg',
+          name: 'BLOOD PRESSURE',
+        },
+      ],
+    },
   },
 ];
 
