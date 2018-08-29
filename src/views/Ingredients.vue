07a0ad53c718050b6743959347c02ab031079117
Andrew Hope <ahope@redventures.com>
2018-08-28 20:05:43 -0700

    scafold initial Ingredients view

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,11 +1,29 @@
 <template lang="html">
   <div>
     <h1><i> Essential</i> Nutrients</h1>
+    <ul>
+      <li v-for="ingredient in ingredientsList" :key="ingredient">
+        <p>{{ingredient}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Ingredients',
+  data() {
+    return {
+      ingredientsList: [
+        'folate',
+        'Omega-3',
+        'Vitamin D3',
+        'Iron',
+        'Vitain K2',
+        'Magnesium',
+      ],
+    };
+  },
 };
 </script>
 
