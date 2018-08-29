b63ee4ea29334c47c2b6eaf58b0e6396cb44400a
Andrew Hope <ahope@redventures.com>
2018-08-29 15:58:52 -0700

    move data into its own file

--- src/data.js
+++ src/data.js
@@ -1,5 +1,54 @@
-const data = [
-
+const Data = [
+  {
+    name: 'Folate',
+    amount: '600 MCG',
+    origin: 'Pisticci, Italy',
+    detail: {
+      form: '5MTHF GLUCOSAMINE SALT',
+      manufacturer: 'Gnosis',
+      found: 'Lentils, avocado, and oranges.',
+      image: 'https://5.imimg.com/data5/EX/IU/MY-2290318/folic-acid-250x250.jpg',
+      icons: [
+        {
+          source: 'https://image.flaticon.com/icons/svg/39/39272.svg',
+          name: 'MOOD',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/340/340624.svg',
+          name: 'DNA SYNTHESIS',
+        },
+        {
+          source: 'https://image.flaticon.com/icons/svg/109/109727.svg',
+          name: 'BRAIN HEALTH',
+        },
+      ],
+    },
+  },
+  {
+    name: 'Omega-3',
+    amount: '320 MG',
+    origin: 'South Carolina, USA',
+  },
+  {
+    name: 'Vitamin D3',
+    amount: '2000 IU',
+    origin: 'United Kingdom',
+  },
+  {
+    name: 'Iron',
+    amount: '8 MG',
+    origin: 'Utah, USA',
+  },
+  {
+    name: 'Vitamin K2',
+    amount: '90 MCG',
+    origin: 'Oslo, Norway',
+  },
+  {
+    name: 'Magnesium',
+    amount: '50 MG',
+    origin: 'Pisa, Italy',
+  },
 ];
 
-export default data;
+export default Data;
--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -24,6 +24,7 @@
 <script>
 import IngredientCard from '../components/IngredientCard.vue';
 import IngredientDetail from '../components/IngredientDetail.vue';
+import Data from '../data';
 
 export default {
   name: 'Ingredients',
@@ -39,58 +40,7 @@ export default {
   data() {
     return {
       currentIngredientIndex: 0,
-      ingredientsList: [
-        {
-          name: 'Folate',
-          amount: '600 MCG',
-          origin: 'Pisticci, Italy',
-          detail: {
-            form: '5MTHF GLUCOSAMINE SALT',
-            manufacturer: 'Gnosis',
-            found: 'Lentils, avocado, and oranges.',
-            image: 'https://5.imimg.com/data5/EX/IU/MY-2290318/folic-acid-250x250.jpg',
-            icons: [
-              {
-                source: 'https://image.flaticon.com/icons/svg/39/39272.svg',
-                name: 'MOOD',
-              },
-              {
-                source: 'https://image.flaticon.com/icons/svg/340/340624.svg',
-                name: 'DNA SYNTHESIS',
-              },
-              {
-                source: 'https://image.flaticon.com/icons/svg/109/109727.svg',
-                name: 'BRAIN HEALTH',
-              },
-            ],
-          },
-        },
-        {
-          name: 'Omega-3',
-          amount: '320 MG',
-          origin: 'South Carolina, USA',
-        },
-        {
-          name: 'Vitamin D3',
-          amount: '2000 IU',
-          origin: 'United Kingdom',
-        },
-        {
-          name: 'Iron',
-          amount: '8 MG',
-          origin: 'Utah, USA',
-        },
-        {
-          name: 'Vitamin K2',
-          amount: '90 MCG',
-          origin: 'Oslo, Norway',
-        },
-        {
-          name: 'Magnesium',
-          amount: '50 MG',
-          origin: 'Pisa, Italy',
-        },
-      ],
+      ingredientsList: Data,
     };
   },
   methods: {
